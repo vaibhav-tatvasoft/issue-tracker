f4282a9ece724e7eea14146826cc99ba797d0624
Vaibhav Panchal <vaibhav.panchal@tatvasoft.com>
2024-11-12 15:07:26 +0530

    Added userId provider

---
+++ ASP.NET Projects/ChattingApplication/ChattingApplication.Main/Services/CustomUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChattingApplication.Main.Services
+{
+    public class CustomUserIdProvider : IUserIdProvider
+    {
+        public string? GetUserId(HubConnectionContext connection)
+        {
+            var userId = connection.GetHttpContext()?.Request.Query["userId"];
+            return userId;
+        }
+    }
+}
--- ASP.NET Projects/ChattingApplication/ChattingApplication.Main/Services/RealTimeHub.cs
+++ ASP.NET Projects/ChattingApplication/ChattingApplication.Main/Services/RealTimeHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChattingApplication.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChattingApplication.Main.Services
 {
@@ -6,8 +7,12 @@ namespace ChattingApplication.Main.Services
     {
         public async Task SendMessage(string user, string message)
         {
-            //await Clients.All.SendAsync("ReceiveMessage",user, message);
-            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+
+            Console.WriteLine(Clients.All.ToString);
+
+            await Clients.User(user).SendAsync("ReceiveMessage", Context.UserIdentifier, message);
         }
 
         public async Task SendNotification(string message)
