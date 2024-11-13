dab3c7399b8e2b236a70e175833d0dc3e4d82a60
Vaibhav Panchal <vaibhav.panchal@tatvasoft.com>
2024-11-13 17:57:29 +0530

    Added message object

--- ASP.NET Projects/ChattingApplication/ChattingApplication.Main/Services/RealTimeHub.cs
+++ ASP.NET Projects/ChattingApplication/ChattingApplication.Main/Services/RealTimeHub.cs
@@ -1,6 +1,8 @@
 ﻿using ChattingApplication.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using System.Collections.Concurrent;
+using System.Text.Json;
 
 namespace ChattingApplication.Main.Services
 {
@@ -15,6 +17,9 @@ namespace ChattingApplication.Main.Services
             Console.WriteLine($"Client connected: {Context.ConnectionId}");
             Console.WriteLine(ConnectedClients.Count);
             Console.WriteLine(ConnectedClients.ToString);
+
+            SendAllClientsList(ConnectedClients.Keys.ToList());
+
             return base.OnConnectedAsync();
         }
 
@@ -24,22 +29,22 @@ namespace ChattingApplication.Main.Services
             ConnectedClients.Remove(Context.ConnectionId, out _);
             Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
 
+            SendAllClientsList(ConnectedClients.Keys.ToList());
+
             return base.OnDisconnectedAsync(exception);
         }
 
-        public async Task SendAllClientsList()
+        public async Task SendAllClientsList(List<string> users)
         {
-            await Clients.User(Context.UserIdentifier).SendAsync("ReceiveAllClientsList", ConnectedClients);
+            await Clients.Clients(users).SendAsync("ReceiveAllClientsList", ConnectedClients);
         }
 
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, Object message) //user who SENT the message and message object
         {
             //await Clients.All.SendAsync("ReceiveMessage", user, message);
             //await Clients.Caller.SendAsync("ReceiveMessage", user, message);
-
-            Console.WriteLine(Clients.All.ToString);
-
-            await Clients.User(user).SendAsync("ReceiveMessage", Context.UserIdentifier, message);
+            var messageObject = JsonSerializer.Deserialize<Message>(message.ToString());
+            await Clients.User(user).SendAsync("ReceiveMessage", Context.UserIdentifier, messageObject);
         }
 
         public async Task SendNotification(string message)
---
+++ ASP.NET Projects/ChattingApplication/ChattingApplication.Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattingApplication.Models
+{
+    public class Message
+    {
+        public string content { get; set; }
+        public string timestamp { get; set; }
+        public string to { get; set; }
+        public string from { get; set; }
+    }
+}
